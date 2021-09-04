7b601e710f7d547367bd14012c80e8e1b1c0b5ca
ahmadfadillllah <ahmadfadillah502@gmail.com>
2021-09-04 17:44:58 +0800

    Update Halaman Produk

--- resources/views/Admin/master.blade.php
+++ resources/views/Admin/master.blade.php
@@ -2564,14 +2564,14 @@
                         <!--begin::Copyright-->
                         <div class="text-dark order-2 order-md-1">
                             <span class="text-muted fw-bold me-1">2021©</span>
-                            <a href="https://keenthemes.com" target="_blank"
-                                class="text-gray-800 text-hover-primary">Keenthemes</a>
+                            <a href="javascript:void(0);" target="_blank"
+                                class="text-gray-800 text-hover-primary">Sayur-sayuran</a>
                         </div>
                         <!--end::Copyright-->
                         <!--begin::Menu-->
                         <ul class="menu menu-gray-600 menu-hover-primary fw-bold order-1">
                             <li class="menu-item">
-                                <a href="https://keenthemes.com" target="_blank" class="menu-link px-2">About</a>
+                                <a href="javascript:void(0);" class="menu-link px-2">About</a>
                             </li>
                             <li class="menu-item">
                                 <a href="https://keenthemes.com/support" target="_blank"
